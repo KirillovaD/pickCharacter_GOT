782791a54bd4119d40ddbc60e3f71fd067183bfe
Irina Finogenova <finogenova123@rambler.ru>
2022-09-21 09:08:49 +0300

    Добавила верстку 1 страницы

--- src/page1/index.js
+++ src/page1/index.js
@@ -1,1 +1,12 @@
 console.log('Hello')
+
+let radio = new Audio();
+radio.src = "assets/audio.mp3";
+
+document.querySelector('#on').onclick = function() {
+    radio.play()
+}
+document.querySelector('#off').onclick = function() {
+    radio.pause()
+}
+
