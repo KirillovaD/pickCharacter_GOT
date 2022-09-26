48b1d3ca33fb7ddece1c8b89a799670c12cc198f
KirillovaD <sdaria@inbox.ru>
2022-09-26 16:23:12 +0300

    add funct in modal carousel

--- src/page1/index.js
+++ src/page1/index.js
@@ -157,17 +157,22 @@ console.log(id);
 const inputs = document.querySelectorAll("input");
 const modalNode = document.querySelectorAll(".modalQuestions");
 
-console.log(inputs)
-for (let i = 0; i < inputs.length; i++ ){
-    console.log(inputs[i]);
+let i
+for (i = 0; i < inputs.length; i++ ){
+    console.log(inputs[i])
     inputs[i].addEventListener("click", onCheckNext);
-    
 } 
+let current = 0;
 function onCheckNext(){
+    if (current +1 == modalNode.length && inputs[i].checked == true){
+        calc.classList.add(":hover");
+      } else {
+        current ++;
+    }
     for (let i = 0; i < modalNode.length; i++){
-        modalNode[i].classList.add("modalQuestions_hidden");
-      }
-      modalNode[current].classList.remove("modalQuestions_hidden");
+        modalNode[i].classList.remove("modalQuestions_active");
+    }
+    modalNode[current].classList.add("modalQuestions_active");
 
 }
 
